b6cb6737608b98b6b4f10f8e2781c5598ce97457
Jarosław Terejko <j.terejko@gmail.com>
2018-09-20 10:29:58 +0200

    changed data structure, added new currencies

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const btn = document.querySelector('#submit-btn');
+const form = document.querySelector('#currency-form');
 const content = document.querySelector('.content');
 
 const displayResult = document.createElement('p');
-displayResult.classList.add('result', 'text-primary', 'text-center', 'mt-md-5');
+displayResult.classList.add('result', 'text-primary', 'text-center');
 content.appendChild(displayResult);
 
-const PLNEUR = 4.24;
-const EURPLN = .26;
-const PLNUSD = .42;
-const USDPLN = 3.63;
-const EURUSD = 1.21;
-const USDEUR = .83;
+const exchangeRates = {
+	plnEur: 4.29,
+	eurPln: .23,
+	plnUsd: 3.66,
+	UsdPln: .27,
+	plnGbp: 4.84,
+	gbpPln: .2,
+	plnChf: 3.8,
+	chfPln: .26,
+	eurUsd: .85,
+	usdEur: 1.13,
+	eurGbp: 1.12,
+	gbpEur: .88,
+	eurChf: .9,
+	chfEur: 1.13,
+	usdGbp: 1.32,
+	gbpUsd: .75,
+	usdChf: 1.03,
+	chfUsd: .97
+}
 
-btn.addEventListener('click', function (e) {
-    e.preventDefault();
+const { plnEur, eurPln, plnUsd, UsdPln, plnGbp, gbpPln, plnChf, chfPln, eurUsd, usdEur, eurGbp, gbpEur, eurChf, chfEur, usdGbp, gbpUsd, usdChf, chfUsd } = exchangeRates;
+
+const convertCurrency = (e) => {
+	
+	e.preventDefault();
 
     const inputAmount = document.querySelector('#input-amount').value;
     const currencyFrom = document.querySelector('#currency-from').value;
@@ -21,30 +38,58 @@ btn.addEventListener('click', function (e) {
 
     let result;
     if (currencyFrom === 'PLN' && currencyTo === 'EUR') {
-        result = inputAmount * EURPLN;
+        result = inputAmount * eurPln;
     } else if (currencyFrom === 'EUR' && currencyTo === 'PLN') {
-        result = inputAmount * PLNEUR;
+        result = inputAmount * plnEur;
     } else if (currencyFrom === 'PLN' && currencyTo === 'USD') {
-        result = inputAmount * PLNUSD;
+        result = inputAmount * UsdPln;
     } else if (currencyFrom === 'USD' && currencyTo === 'PLN') {
-        result = inputAmount * USDPLN;
+        result = inputAmount * plnUsd;
+    } else if (currencyFrom === 'PLN' && currencyTo === 'GBP') {
+        result = inputAmount * gbpPln;
+    } else if (currencyFrom === 'GBP' && currencyTo === 'PLN') {
+        result = inputAmount * plnGbp;
+    } else if (currencyFrom === 'PLN' && currencyTo === 'CHF') {
+        result = inputAmount * chfPln;
+    } else if (currencyFrom === 'CHF' && currencyTo === 'PLN') {
+        result = inputAmount * plnChf;
     } else if (currencyFrom === 'EUR' && currencyTo === 'USD') {
-        result = inputAmount * EURUSD;
+        result = inputAmount * usdEur;
     } else if (currencyFrom === 'USD' && currencyTo === 'EUR') {
-        result = inputAmount * USDEUR;
+        result = inputAmount * eurUsd;
+    } else if (currencyFrom === 'EUR' && currencyTo === 'GBP') {
+        result = inputAmount * gbpEur;
+    } else if (currencyFrom === 'GBP' && currencyTo === 'EUR') {
+        result = inputAmount * eurGbp;
+    } else if (currencyFrom === 'EUR' && currencyTo === 'CHF') {
+        result = inputAmount * chfEur;
+    } else if (currencyFrom === 'CHF' && currencyTo === 'EUR') {
+        result = inputAmount * eurChf;
+    } else if (currencyFrom === 'USD' && currencyTo === 'GBP') {
+        result = inputAmount * gbpUsd;
+    } else if (currencyFrom === 'GBP' && currencyTo === 'USD') {
+        result = inputAmount * usdGbp;
+    } else if (currencyFrom === 'USD' && currencyTo === 'CHF') {
+        result = inputAmount * chfUsd;
+    } else if (currencyFrom === 'CHF' && currencyTo === 'USD') {
+        result = inputAmount * usdChf;
     } else {
         result = inputAmount * 1;
-
     }
 
     if (inputAmount === '') {
 
-        displayResult.textContent = `Wpisz kwotę jaką dysponujesz abu otrzymać wynik`;
+        displayResult.textContent = `Wpisz kwotę jaką dysponujesz, aby otrzymać wynik`;
 
     } else {
+		
         result = result.toFixed(2);
-
+		
         displayResult.textContent = `Kwota ${inputAmount} ${currencyFrom} pozwoli Ci na zakup ${result} ${currencyTo}`;
+		document.querySelector('#input-amount').value = '';
+		
     }
 
-});
+}
+
+form.addEventListener('submit', convertCurrency);
